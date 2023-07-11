b58336a581305843fc3fdd45c47bb51279220d54
Rohan Jalil <rohan.jalil@gmail.com>
2023-07-11 13:42:09 +0500

    Updated readme and integrated streamlit

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
-from langchain.agents.agent_types import AgentType
+import os
 
-agent = create_csv_agent(
-    OpenAI(temperature=0),
-    "./files_to_read/test.csv",
-    verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.vectorstores import DocArrayInMemorySearch
+
+load_dotenv()
+
+loader = CSVLoader(
+    file_path=os.path.join("files_to_read", "test.csv"), encoding="utf-8"
+)
+data = loader.load()
+
+embeddings = OpenAIEmbeddings()
+vector_store = DocArrayInMemorySearch.from_documents(data, embeddings)
+
+qa = RetrievalQA.from_chain_type(
+    llm=OpenAI(),
+    chain_type="stuff",
+    retriever=vector_store.as_retriever(),
+    return_source_documents=True,
 )
 
-agent.run("How many totals rows are there in the dataset?")
+question = "How many totals rows are there in the dataset?"
+result = qa({"query": question})
+
+print(result)
